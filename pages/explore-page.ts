3bb4b5e85be11404ec9967d7eda80ff279e7777e
KaushikTest <kaushik6990@gmail.com>
2025-09-08 15:55:27 +0530

    Create explore-page.ts

---
+++ pages/explore-page.ts
@@ -0,0 +1,41 @@
+import { Page } from "@playwright/test";
+
+export default class ExplorePage {
+    private page: Page;
+
+    constructor(page: Page) {
+        this.page = page;
+    }
+
+    public get ExploreQuestions() {
+        return this.page.getByRole('button', { name: 'Explore Questions' });
+    }
+
+    public get StartLearning() {
+        return this.page.getByRole('button', { name: 'Start Learning' });
+    }
+
+    public get TakeChallenge() {
+        return this.page.getByRole('button', { name: 'Take Challenge' });
+    }
+
+    public get ReadBlogs() {
+        return this.page.getByRole('button', { name: 'Read Blogs' });
+    }
+
+    public async clickExploreQuestions() {
+        return this.ExploreQuestions.click();
+    }
+
+    public async clickStartLearning() {
+        return this.StartLearning.click();
+    }
+
+    public async clickTakeChallenge() {
+        return this.TakeChallenge.click();
+    }
+
+    public async clickReadBlogs() {
+        return this.ReadBlogs.click();
+    }
+}
