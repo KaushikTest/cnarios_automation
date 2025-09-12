e33c65b4a629bcd20e88be6a2fd02ff2ffb713ef
KaushikTest <kaushik6990@gmail.com>
2025-09-13 01:34:47 +0530

    Update explorepage-fixture.ts

--- fixtures/explorepage-fixture.ts
+++ fixtures/explorepage-fixture.ts
@@ -1,9 +1,8 @@
-import { test as baseTest } from "@playwright/test";
+import { test as baseTest } from "../fixtures/homepage-fixture";
 import * as HomePageCases from "../cases/homepage-cases";
 
 export const test = baseTest.extend<{ exploredHomepage: void }>({
-    exploredHomepage: async ({ page }, use) => {
-        await page.goto("https://www.cnarios.com/");
+    exploredHomepage: async ({ page, homePage }, use) => {
         await HomePageCases.clickStartExploringAtHomepage(page);
         await use(undefined);
     },
