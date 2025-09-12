ed95d9851c262e66bcc3f6e1b6204809a44fe9f8
KaushikTest <kaushik6990@gmail.com>
2025-09-13 00:14:15 +0530

    Update challengespage-fixture.ts

--- fixtures/challengespage-fixture.ts
+++ fixtures/challengespage-fixture.ts
@@ -1,12 +1,10 @@
 import { test as baseTest } from "@playwright/test";
 import * as HomePageCases from "../cases/homepage-cases";
-import * as ExplorePageCases from '../cases/explorepage-cases';
 
-export const test = baseTest.extend<{ conceptsHomePage: void }>({
-    conceptsHomePage: async ({ page }, use) => {
+export const test = baseTest.extend<{ challengesPage: void }>({
+    challengesPage: async ({ page }, use) => {
         await page.goto("https://www.cnarios.com/");
-        await HomePageCases.clickStartExploringAtHomepage(page);
-        await ExplorePageCases.clickConcepts(page);
+        await HomePageCases.clickChallengesAtHomepage(page);
         await use(undefined);
     },
 });
