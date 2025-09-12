e919fed9a48d15c0d4b78bfc1382307b9477c577
KaushikTest <kaushik6990@gmail.com>
2025-09-13 00:13:34 +0530

    Create button-page.ts

---
+++ pages/button-page.ts
@@ -0,0 +1,26 @@
+import { Page } from "@playwright/test";
+
+export default class Button {
+    private page: Page;
+
+    constructor(page: Page) {
+        this.page = page;
+    }
+
+    public get TryItYourself() {
+        return this.page.getByRole('tab', { name: 'Try It Yourself' });
+    }
+
+    public get TestCases() {
+        return this.page.getByRole('tab', { name: 'Test Cases' })
+    }
+
+    public async clickTryItYourself() {
+        await this.TryItYourself.click();
+    }
+
+    public async clickTestCases() {
+        await this.TestCases.click();
+    }
+
+}
