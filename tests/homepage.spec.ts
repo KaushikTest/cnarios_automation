0e19a7f941d3edcac7f334d7d2742737242e63b8
KaushikTest <kaushik6990@gmail.com>
2025-09-13 01:34:59 +0530

    Update homepage.spec.ts

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -1,13 +1,11 @@
-import test, { expect } from "@playwright/test";
+import { expect } from "@playwright/test";
 import * as HomePageCases from '../cases/homepage-cases';
+import { test } from "../fixtures/homepage-fixture";
 
-test.beforeEach(async ({ page }) => {
-    await page.goto('https://www.cnarios.com/');
-})
 
 test.describe('HomePage Tests', async () => {
 
-    test('Validate Scroll to Features', async ({ page }) => {
+    test('Validate Scroll to Features', async ({ page, homePage }) => {
         await HomePageCases.clickFeaturesAtHomepage(page);
         const element = page.getByText('Hands-On Practice');
         await element.scrollIntoViewIfNeeded({ timeout: 5000 });
@@ -16,7 +14,7 @@ test.describe('HomePage Tests', async () => {
         await expect(element).toBeInViewport();
     })
 
-    test('Validate Scroll to How it works? ', async ({ page }) => {
+    test('Validate Scroll to How it works? ', async ({ page, homePage }) => {
         await HomePageCases.clickHowItWorksAtHomepage(page);
         const element = page.locator('p', { hasText: 'Search for the topic you want to master' });
         await element.scrollIntoViewIfNeeded({ timeout: 5000 });
@@ -26,7 +24,7 @@ test.describe('HomePage Tests', async () => {
 
     })
 
-    test('Validate Scroll to Contact Us ', async ({ page }) => {
+    test('Validate Scroll to Contact Us ', async ({ page, homePage }) => {
         await HomePageCases.clickContactUsAtHomePage(page);
         const element = page.getByRole('link', { name: 'Email' })
         await element.scrollIntoViewIfNeeded({ timeout: 5000 });
@@ -36,7 +34,7 @@ test.describe('HomePage Tests', async () => {
 
     })
 
-    test('Validate Navigation to Blogs', async ({ page }) => {
+    test('Validate Navigation to Blogs', async ({ page, homePage }) => {
         await HomePageCases.clickBlogsAtHomePage(page);
         const element = page.getByText('Latest Blogs & Insights');
         expect(page.url()).toEqual('https://www.cnarios.com/blogs');
@@ -45,7 +43,7 @@ test.describe('HomePage Tests', async () => {
 
     })
 
-    test('Validate Navigation to Explore', async ({ page }) => {
+    test('Validate Navigation to Explore', async ({ page, homePage }) => {
         await HomePageCases.clickStartExploringAtHomepage(page);
         const element = page.getByText('Explore. Practice. Master.');
         expect(page.url()).toEqual('https://www.cnarios.com/explore');
@@ -54,7 +52,7 @@ test.describe('HomePage Tests', async () => {
 
     })
 
-    test('Validate Navigation to Challenges', async ({ page }) => {
+    test('Validate Navigation to Challenges', async ({ page, homePage }) => {
         await HomePageCases.clickChallengesAtHomepage(page);
         const element = page.getByText('Automation Challenges');
         expect(page.url()).toEqual('https://www.cnarios.com/challenges');
@@ -63,3 +61,4 @@ test.describe('HomePage Tests', async () => {
     })
 
 })
+
