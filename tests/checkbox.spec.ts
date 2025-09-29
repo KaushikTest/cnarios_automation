eecf154acf7b1bcbfbf9e10b95e6782d40cac638
KaushikTest <kaushik6990@gmail.com>
2025-09-30 00:26:02 +0530

    Create checkbox.spec.ts

---
+++ tests/checkbox.spec.ts
@@ -0,0 +1,36 @@
+import { expect } from "@playwright/test";
+import * as CheckboxCases from '../cases/buttonpage-cases';
+import { uncheckDefault } from "../commons/helper";
+import { test } from "../fixtures/checkboxpage-fixture";
+
+test.describe('Checkbox Tests', async () => {
+    test.describe.configure({ mode: "serial" });
+    test('Validate Navigation to Try It Yourself', async ({ page, checkboxPage }) => {
+        await CheckboxCases.clickTryItYourself(page);
+        const element = page.getByRole('heading', { name: 'News Feed Cnario' });
+        await element.scrollIntoViewIfNeeded({ timeout: 5000 });
+        expect(page.url()).toEqual('https://www.cnarios.com/concepts/checkbox#try-it-yourself');
+        await expect(element).toBeVisible();
+        await expect(element).toBeInViewport();
+    })
+
+    test('Validate the default category feed selected', async ({ page, checkboxPage }) => {
+        await CheckboxCases.clickTryItYourself(page);
+        expect(page.url()).toEqual('https://www.cnarios.com/concepts/checkbox#try-it-yourself');
+        await expect(page.getByText('Category: Technology')).toBeVisible();
+        await expect(page.getByText('Category: Sports')).toBeVisible();
+    })
+
+    const categories = ['Technology', 'Sports', 'Business', 'Science', 'Health', 'Entertainment'];
+
+    categories.forEach((element: string) => {
+        test(`Validate the selection of category ${element}`, async ({ page, checkboxPage }) => {
+            await CheckboxCases.clickTryItYourself(page);
+            await uncheckDefault(page);
+            await page.getByRole('checkbox', { name: element }).check();
+            await page.getByRole('button', { name: 'Done' }).click();
+            await expect(page.getByText(`Category: ${element}`)).toBeVisible();
+        })
+
+    })
+})
