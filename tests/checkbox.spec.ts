690dc3195afa62993396c002a2b10aa0470c6fc5
KaushikTest <kaushik6990@gmail.com>
2025-10-01 07:02:57 +0530

    Update checkbox.spec.ts

--- tests/checkbox.spec.ts
+++ tests/checkbox.spec.ts
@@ -33,4 +33,19 @@ test.describe('Checkbox Tests', async () => {
         })
 
     })
+
+    const two_categories = [['Technology', 'Sports'], ['Sports', 'Business'], ['Business', 'Science'], ['Science', 'Health'], ['Health', 'Entertainment'], ['Technology', 'Entertainment']];
+
+    two_categories.forEach(([category1, category2]) => {
+        test(`Validate the selection of two_category ${category1} & ${category2}`, async ({ page, checkboxPage }) => {
+            await CheckboxCases.clickTryItYourself(page);
+            await uncheckDefault(page);
+            await page.getByRole('checkbox', { name: category1 }).check();
+            await page.getByRole('checkbox', { name: category2 }).check();
+            await page.getByRole('button', { name: 'Done' }).click();
+            await expect(page.getByText(`Category: ${category1}`)).toBeVisible();
+            await expect(page.getByText(`Category: ${category2}`)).toBeVisible();
+        })
+    })
+
 })
