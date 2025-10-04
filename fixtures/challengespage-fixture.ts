4622f233574824aa1921e130e53d169cb5b9fbd2
KaushikTest <kaushik6990@gmail.com>
2025-10-04 23:58:17 +0530

    Update challengespage-fixture.ts

--- fixtures/challengespage-fixture.ts
+++ fixtures/challengespage-fixture.ts
@@ -3,7 +3,7 @@ import * as HomePageCases from "../cases/homepage-cases";
 
 export const test = baseTest.extend<{ challengesPage: void }>({
     challengesPage: async ({ page }, use) => {
-        await page.goto("https://www.cnarios.com/");
+        await page.goto("https://www.cnarios.com/", { timeout: 5000 });
         await HomePageCases.clickChallengesAtHomepage(page);
         await use(undefined);
     },
